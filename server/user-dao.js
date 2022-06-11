e04608c4f7a329cde94ad661ed3cd7d3017277f9
Manuel Pepe <manuelpepe1995@gmail.com>
2022-06-11 10:24:24 +0200

    removed console log from server

--- server/user-dao.js
+++ server/user-dao.js
@@ -30,7 +30,6 @@ exports.getUser = (email, password) => {
     return new Promise((resolve, reject) => {
       const sql = 'SELECT * FROM users WHERE email = ?';
       db.get(sql, [email], (err, row) => {
-        console.log(`row: ${JSON.stringify(row)}`);
 
         if (err) { reject(err); }
         else if (row === undefined) { resolve(false); }
