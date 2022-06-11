6404aa529b54edf51c98ebcfa267f60799c7f3a4
Manuel Pepe <manuelpepe1995@gmail.com>
2022-06-11 10:23:04 +0200

    update login controls

--- client/src/LoginComponents.js
+++ client/src/LoginComponents.js
@@ -4,53 +4,66 @@ import { useState } from 'react';
 function LoginForm(props) {
   const [username, setUsername] = useState('testuser@polito.it');
   const [password, setPassword] = useState('password');
-  const [errorMessage, setErrorMessage] = useState('') ;
-  
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [validated, setValidated] = useState(false);
+
   const handleSubmit = (event) => {
-      event.preventDefault();
-      setErrorMessage('');
-      const credentials = { username, password };
-      
-      // SOME VALIDATION, ADD MORE!!!
-      let valid = true;
-      if(username === '' || password === '')
-          valid = false;
-      
-      if(valid)
-      {
-        props.login(credentials);
-      }
-      else {
-        // show a better error message...
-        setErrorMessage('Error(s) in the form, please fix it.')
-      }
+    event.preventDefault();
+    event.stopPropagation();
+
+    const credentials = { username, password };
+
+    const form = event.currentTarget;
+
+    if (!form.checkValidity()) {
+      setValidated(true);
+    }
+    else if (username === '' || password === '') {
+      props.setMessage("Email and password cannot be empty ")
+      setValidated(false);
+    }
+    else {
+      props.login(credentials);
+    }
   };
 
   return (
-      <Container>
-          <Row>
-              <Col>
-                  <h2>Login</h2>
-                  <Form>
-                      {errorMessage ? <Alert variant='danger'>{errorMessage}</Alert> : ''}
-                      <Form.Group controlId='username'>
-                          <Form.Label>email</Form.Label>
-                          <Form.Control type='email' value={username} onChange={ev => setUsername(ev.target.value)} />
-                      </Form.Group>
-                      <Form.Group controlId='password'>
-                          <Form.Label>Password</Form.Label>
-                          <Form.Control type='password' value={password} onChange={ev => setPassword(ev.target.value)} />
-                      </Form.Group>
-                      <Button onClick={handleSubmit}>Login</Button>
-                  </Form>
-              </Col>
-          </Row>
-      </Container>
-    )
+    <Container>
+      <Row>
+        <Col>
+          <h2>Login</h2>
+          <Form noValidate validated={validated} onSubmit={handleSubmit} >
+            {errorMessage ? <Alert variant='danger'>{errorMessage}</Alert> : ''}
+            <Form.Group controlId='username'>
+              <Form.Label>email</Form.Label>
+              <Form.Control
+                type='email'
+                hasvalidation={"true"}
+                value={username}
+                onChange={ev => setUsername(ev.target.value)}
+                required />
+              <Form.Control.Feedback type="invalid">
+                Please choose a valid email
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' hasvalidation={"true"} value={password} onChange={ev => setPassword(ev.target.value)} required />
+              <Form.Control.Feedback type="invalid">
+                Please insert password
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit">Login</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 function LogoutButton(props) {
-  return(
+  return (
     <Col>
       <span>Welcome, {props.user?.name}</span>{' '}<Button variant="outline-primary" onClick={props.logout}>Logout</Button>
     </Col>
